3ceffc93739f93cabc1acae2a0d59a9b779c0524
artem <ip.bakhmatov@mail.ru>
2023-01-15 18:04:41 +0300

    Сборка и архитектура проекта

---
+++ src/js/lib/core.js
@@ -0,0 +1,42 @@
+// (() => {
+//     const $ = function(selector) {
+//         const elements = document.querySelectorAll(selector);
+//         const obj = {};
+        
+//         obj.hide = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = 'none';
+//             });
+//             return obj;
+//         };
+
+//         obj.show = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = '';
+//             });
+//             return obj;
+//         };
+
+//         return obj;
+//     }
+//     window.$ = $;
+// })();
+
+const $ = function(selector) {
+    return new $.prototype.init(selector);
+};
+
+$.prototype.init = function(selector) {
+    if (!selector) {
+        return this;  // будет просто пустой объект
+    }
+    Object.assign(this, document.querySelectorAll(selector));  // в существ-й объект добавить новое свойство
+    this.length = document.querySelectorAll(selector).length;
+    return this;
+};
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
