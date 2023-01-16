764e0a877bb18b0a2b6931dc702003b8e3012130
artem <ip.bakhmatov@mail.ru>
2023-01-16 18:31:11 +0300

    Создаем методы для работы с элементами. Часть 1

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -1,34 +1,70 @@
-import $ from "../core";
+import $ from "../core"; 
 
-$.prototype.on = function(eventName, callback) {
-    if (!eventName || !callback) {
-        return this;
-    }
-    for(let i = 0; i < this.length; i++) {
-        this[i].addEventListener(eventName, callback);
-    }
-
-    return this;
-};
-
-$.prototype.off = function(eventName, callback) {
-    if (!eventName || !callback) {
-        return this;
-    }
-    for(let i = 0; i < this.length; i++) {
-        this[i].removeEventListener(eventName, callback);
-    }
-
-    return this;
-};
-
-$.prototype.click = function(handler) {
-    for(let i = 0; i < this.length; i++) {
-        if (handler) {
-            this[i].addEventListener('click', handler);
+$.prototype.html = function(content) {
+    for (let i = 0; i < this.length; i++) {
+        if (content) {
+            this[i].innerHTML = content;
         } else {
-            this[i].click();
-        }   
+            return this[i].innerHTML;
+        }
+    }
+
+    return this;
+};
+
+$.prototype.eq = function(i) {
+    const swap = this[i];
+    const objLength = Object.keys(this).length; // Объект превращаем в массив и добавляем метод длина
+
+    for (let i = 0; i < objLength; i++) {
+        delete this[i];
+    }
+
+    this[0] = swap;
+    this.length = 1;
+
+    return this;
+};
+
+$.prototype.index = function() {
+    const parent = this[0].parentNode;
+    const childs = [...parent.children];
+
+    const findMyIndex = (item) => {
+        return item == this[0];
+    };
+
+    return childs.findIndex(findMyIndex);
+    // findIndex() -> возвращает индекс в массиве, если элемент удовлетворяет условию проверяющей функции. 
+    // В противном случае возвращается -1.
+};
+
+$.prototype.find = function(selector) {
+    let numberOfitems = 0;
+    let counter = 0;
+
+    const copyObj = Object.assign({}, this);
+
+    for (let i = 0; i < copyObj.length; i++) {
+        const arr = copyObj[i].querySelectorAll(selector);
+
+        if (arr.length == 0) {
+            continue;
+        }
+
+        for (let j = 0; j < arr.length; j++) {
+            this[counter] = arr[j];
+            counter++;
+        }  
+        
+        numberOfitems += arr.length;
+    }
+
+    this.length = numberOfitems;
+
+    const objLength = Object.keys(this).length;
+    for (; numberOfitems < objLength; numberOfitems++) {
+        delete this[numberOfitems];
     }
 
     return this;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).toggleClass('active');
+    $('div').eq(0).toggleClass('active');
 });
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.some'));
+
+console.log($('button').html('Hello!'));
 
 
